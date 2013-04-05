43247bcbf8c4436981fec0fd1f41af0233a04a13
Nishant Samel <nishants@weboniselab.com>
2013-04-05 18:59:18 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+class HomeController < ApplicationController
+  require "google_drive"
+
+  def index
+
+  end
+
+  def create_spreadsheet
+    session = GoogleDrive.login(USERNAME, PASSWORD)
+
+    ws = session.spreadsheet_by_key(KEY).worksheets[0]
+
+    ws[1,2] = 'Participant Information'
+    ws[1,7] = 'Parent Information'
+
+    ws[2,1] = 'First Name'
+    ws[2,2] = 'Last Name'
+    ws[2,3] = 'Age'
+    ws[2,4] = 'T-Shirt Size'
+    ws[2,6] = 'Contact number'
+    ws[2,7] = 'Message'
+    ws[2,8] = 'Message'
+
+    ws[ws.num_rows+1,1] = params[:fname]
+    ws[ws.num_rows,2] = params[:lname]
+    ws[ws.num_rows,3] = params[:age]
+    ws[ws.num_rows,4] = params[:tshirt]
+    ws[ws.num_rows,6] = params[:pname]
+    ws[ws.num_rows,7] = params[:email]
+    ws[ws.num_rows,8] = params[:phone]
+
+    # Reloads the worksheet to get changes by other clients.
+    ws.reload()
+
+    # Save the data to Google Spreadsheet
+    if ws.save()
+      flash[:success] = "You are successfully registered to our site !!! "
+    else
+      flash[:error] = "You are not registered to our site. Please fill the registration details !!!"
+    end
+
+    render 'index'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarcelReeceFoundation::Application.config.secret_token = 'f33ea59fcf9d6acf9be969388fa49b6514ebd2f6d81793685869e7e9e3b98f2c901a1b43b0556075fff557290bbfee96f27de3f0b4e5bb328837027e1def9d26'
